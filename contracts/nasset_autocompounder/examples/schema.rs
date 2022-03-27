73ce15935775eddd7f13bc18901aa02b8b8c6c23
Stanislav Pirx <stanislav.pirx@gmail.com>
2022-03-27 10:56:04 +0700

    rename packagename a bit
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- contracts/nasset_autocompounder/examples/schema.rs
+++ contracts/nasset_autocompounder/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use nasset_autocompounder_nasset_autocompounder::msg::{
+use nasset_autocompounder::msg::{
     AutoNassetValueResponse, ConfigResponse, Cw20HookMsg, ExecuteMsg, GovernanceMsg,
     InstantiateMsg, QueryMsg,
 };
