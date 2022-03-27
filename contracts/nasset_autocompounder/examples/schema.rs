a9b7a25b111daa750ab4bba3e921431cd07461d4
Stanislav Pirx <stanislav.pirx@gmail.com>
2022-03-27 10:29:04 +0700

    add schemas
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- contracts/nasset_autocompounder/examples/schema.rs
+++ contracts/nasset_autocompounder/examples/schema.rs
@@ -2,11 +2,21 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use nasset_autocompounder_nasset_autocompounder::msg::{
+    AutoNassetValueResponse, ConfigResponse, Cw20HookMsg, ExecuteMsg, GovernanceMsg,
+    InstantiateMsg, QueryMsg,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema");
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
-    //TODO: add messages
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(GovernanceMsg), &out_dir);
+    export_schema(&schema_for!(Cw20HookMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(ConfigResponse), &out_dir);
+    export_schema(&schema_for!(AutoNassetValueResponse), &out_dir);
 }
